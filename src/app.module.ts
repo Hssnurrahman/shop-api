989c446795f493a49e41d5b8a80df1fed1d2239b
Hssnurrahman <54182451+Hssnurrahman@users.noreply.github.com>
2022-01-18 23:08:45 +0500

    hard coded mongodb uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local'],
     }),
-    MongooseModule.forRoot(
-      'mongodb+srv://shopapi:shopapi@shopapi.m2xr5.mongodb.net/ShopApi?retryWrites=true&w=majority',
-      { useFindAndModify: false },
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URI, { useFindAndModify: false }),
     ArticleModule,
   ],
   controllers: [AppController],
